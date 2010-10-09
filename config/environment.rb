b9752da885cd24fa9f41f205613c08c2fb5bd550
Evan <evan@evan-desktop.(none)>
2010-10-09 14:41:36 -0500

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+RAILS_GEM_VERSION = '3.0.0' unless defined? RAILS_GEM_VERSION
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Imageboard::Application.initialize!
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imageboard::Application.config.secret_token = '752adf1629f1e6a1a7fb64a75386683b3c08edfc65d4fcf7cd760b3b381b34ba529995efb2029f0868b8a786f6509b7ff133aadc6088371c1f17f1da4284907c'
